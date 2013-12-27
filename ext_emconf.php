90fd8f5a5aa6b9890f332bc867948be72a3d1021
Daniel Lienert <daniel@lienert.cc>
2013-12-27 06:21:14 +0100

    TASK: Update version

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "yag_theme_library".
  *
- * Auto generated 12-05-2013 19:56
+ * Auto generated 27-12-2013 06:20
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.2.1',
+	'version' => '0.3.0-dev',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-6.0.99',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:12:"ext_icon.gif";s:4:"fef0";s:14:"ext_tables.php";s:4:"fcb4";s:14:"ext_tables.sql";s:4:"d41d";s:21:"ExtensionBuilder.json";s:4:"5386";s:38:"Configuration/TypoScript/constants.txt";s:4:"d41d";s:34:"Configuration/TypoScript/setup.txt";s:4:"12a6";s:50:"Configuration/TypoScript/Themes/Library/General.ts";s:4:"facd";s:59:"Configuration/TypoScript/Themes/Library/ItemList.Extlist.ts";s:4:"1028";s:51:"Configuration/TypoScript/Themes/Library/ItemList.ts";s:4:"cd08";s:48:"Configuration/TypoScript/Themes/Library/Theme.ts";s:4:"61e4";s:54:"Configuration/TypoScript/Themes/LibraryDetail/Theme.ts";s:4:"2ba4";s:40:"Resources/Private/Language/locallang.xml";s:4:"4a6d";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"0bdf";s:41:"Resources/Private/Partials/FilterBox.html";s:4:"63f0";s:61:"Resources/Private/Partials/Filter/Options/TagCloudFilter.html";s:4:"db52";s:56:"Resources/Private/Partials/Image/LightboxImageThumb.html";s:4:"7020";s:43:"Resources/Private/Partials/Pager/Delta.html";s:4:"90a9";s:52:"Resources/Private/Templates/ItemList/DetailList.html";s:4:"6a24";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"7bfb";s:41:"Resources/Public/CSS/BootstrapExtract.css";s:4:"c391";s:30:"Resources/Public/CSS/Theme.css";s:4:"1d2c";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:14:"doc/manual.sxw";s:4:"ec6e";}',
+	'_md5_values_when_last_written' => 'a:24:{s:12:"ext_icon.gif";s:4:"fef0";s:14:"ext_tables.php";s:4:"fcb4";s:14:"ext_tables.sql";s:4:"d41d";s:21:"ExtensionBuilder.json";s:4:"5386";s:38:"Configuration/TypoScript/constants.txt";s:4:"d41d";s:34:"Configuration/TypoScript/setup.txt";s:4:"61ea";s:50:"Configuration/TypoScript/Themes/Library/General.ts";s:4:"7117";s:59:"Configuration/TypoScript/Themes/Library/ItemList.Extlist.ts";s:4:"d42b";s:51:"Configuration/TypoScript/Themes/Library/ItemList.ts";s:4:"cd08";s:48:"Configuration/TypoScript/Themes/Library/Theme.ts";s:4:"61e4";s:56:"Configuration/TypoScript/Themes/LibraryDetail/General.ts";s:4:"f4cd";s:54:"Configuration/TypoScript/Themes/LibraryDetail/Theme.ts";s:4:"19a1";s:40:"Resources/Private/Language/locallang.xml";s:4:"4a6d";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"0bdf";s:41:"Resources/Private/Partials/FilterBox.html";s:4:"63f0";s:61:"Resources/Private/Partials/Filter/Options/TagCloudFilter.html";s:4:"4080";s:56:"Resources/Private/Partials/Image/LightboxImageThumb.html";s:4:"4127";s:43:"Resources/Private/Partials/Pager/Delta.html";s:4:"3bc2";s:52:"Resources/Private/Templates/ItemList/DetailList.html";s:4:"cf4e";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"f0bc";s:41:"Resources/Public/CSS/BootstrapExtract.css";s:4:"205d";s:30:"Resources/Public/CSS/Theme.css";s:4:"7e9d";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:14:"doc/manual.sxw";s:4:"ec6e";}',
 );
 
 ?>
